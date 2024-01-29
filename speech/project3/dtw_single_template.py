0de6a2ca31e687c8cd8cdb5fc249e5eb7c9e4cc3
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-01-29 10:57:41 +0800

    dtw_single_template use all 5 test samples

--- speech/project3/dtw_single_template.py
+++ speech/project3/dtw_single_template.py
@@ -8,7 +8,7 @@ from . import INF_FLOAT32, boosted_mfcc_from_file, single_dtw_search
 
 def recognize_number(number: str, template_mfcc_s, cost_interpretation="min"):
     n_correct = 0
-    for i in range(1, 5, 2):  # TODO: Change to 1 through 10
+    for i in range(1, 10, 2):
         test_mfcc = boosted_mfcc_from_file(f"recordings/{number}{i}.wav")
         least_cost = INF_FLOAT32
         prediction = None
@@ -44,8 +44,12 @@ def main():
         for number in NUMBERS
     ]
 
-    accuracies = [recognize_number(number, template_mfcc_s) for number in NUMBERS]
-    print(f"Correctness: {list(zip(NUMBERS, accuracies))}")
+    accuracies = [recognize_number(number, template_mfcc_s) / 5 for number in NUMBERS]
+    print(
+        f"""Number|{"|".join(NUMBERS)}
+{"|".join("-"for _ in range(len(NUMBERS) + 1))}
+Accuracy|{"|".join(str(a) for a in accuracies)}"""
+    )
 
 
 main() if __name__ == "__main__" else None
