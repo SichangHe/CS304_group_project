b974ada1051af468787a33b7a3bfa882400a24c6
Luyao Wang <lywlywly@gmail.com>
2024-02-07 10:42:18 +0800

    get cumalative string on each step

--- speech/project4/bfs.py
+++ speech/project4/bfs.py
@@ -1,14 +1,9 @@
 from collections import deque
 from typing import List
+
 from speech.project4 import DATA_DIR, read_file
 from speech.project4.lextree import Trie, TrieNode
 from typing import Deque, List, Tuple
-
-
-trie = Trie()
-assert trie.insert("bat")[1]
-assert trie.insert("battle")[1]
-assert trie.insert("banana")[1]
 
 
 def bfs(root):
@@ -22,15 +17,26 @@ def bfs(root):
     while queue:
         node, path = queue.popleft()
         path.append(node.value)
-
         for child in node.children.inner.items():
+            path_copy = path.copy()
+            current_word = "".join(path_copy[1:])
+            print(current_word)
             if child[0][0]:
-                path.append(child[1].value)
-                result.append(path)
+                path_copy.append(child[1].value)
+                result.append(path_copy)
             else:
-                queue.append((child[1], path.copy()))
+                queue.append((child[1], path_copy))
 
     return result
 
 
-print(bfs(trie.root))
+def main():
+    trie = Trie()
+    assert trie.insert("bat")[1]
+    assert trie.insert("battle")[1]
+    assert trie.insert("banana")[1]
+    print(trie)
+    print(bfs(trie.root))
+
+
+main() if __name__ == "__main__" else None
