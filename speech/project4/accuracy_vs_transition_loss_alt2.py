9824dc3f915a04ff360baf6e3937a6f0a4799c28
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-08 12:56:53 +0800

    plot accuracy vs transition loss

---
+++ speech/project4/accuracy_vs_transition_loss_alt2.py
@@ -0,0 +1,40 @@
+"""Run as `python3 -m speech.project4.accuracy_vs_transition_loss_alt2`."""
+
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+
+from . import DATA_DIR, read_lines_stripped
+from .accuracy_vs_beam import experiment_inaccuracy
+from .correct_story import correct_story_lines_stripped
+from .dictionary import dictionary_trie
+from .segment_and_spellcheck_alt2 import alter_dict_trie_losses
+
+plt.rcParams["font.size"] = 24
+
+
+def main() -> None:
+    dict_trie = dictionary_trie()
+    alter_dict_trie_losses(dict_trie)
+
+    lines = read_lines_stripped(f"{DATA_DIR}unsegmented.txt")
+    correct_lines = correct_story_lines_stripped()
+
+    transition_losses = (0x1, 0x2, 0x4, 0x6, 0x8, 0xA, 0xC, 0x10)
+    inaccuracies = []
+    for transition_loss in transition_losses:
+        dict_trie.transition_loss = transition_loss
+        inaccuracy = experiment_inaccuracy(dict_trie, lines, correct_lines, 0x20)
+        inaccuracies.append(inaccuracy)
+
+    ax: Axes
+    fig, ax = plt.subplots()
+    ax.plot(transition_losses, inaccuracies)
+    ax.grid()
+    ax.set_xlabel("Transition Loss")
+    ax.set_ylabel("Inaccuracy")
+    ax.ticklabel_format(style="plain")
+    plt.show(block=True)
+    fig.savefig("accuracy_vs_transition_loss_alt2.png", bbox_inches="tight")
+
+
+main() if __name__ == "__main__" else None
