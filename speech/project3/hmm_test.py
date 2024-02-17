5cfded2dd80383496a4d4fef2025cec1665e04f2
Luyao Wang <lywlywly@gmail.com>
2024-02-18 01:51:12 +0800

    fix project3 hmm test

--- speech/project3/hmm_test.py
+++ speech/project3/hmm_test.py
@@ -3,27 +3,27 @@ import unittest
 
 import numpy as np
 
-from speech.project3.hmm import HMM
+from speech.project3.hmm import HMM, HMM_Single
 
 
 class TestAudio(unittest.TestCase):
     def test_1(self):
-        mean = np.random.normal(0, 1, 40)
-        covariance = np.eye(40)
+        mean = np.random.normal(0, 1, 39)
+        covariance = np.eye(39)
         num_samples = 200
         np.random.seed(0)
         samples = np.random.multivariate_normal(mean, covariance, num_samples)
         s = [samples[i : i + 40] for i in range(0, 161, 10)]
-        dtw = HMM()
-        dtw.fit(s, 5, 1)
+        dtw = HMM_Single(s, 5, 1)
         another_samples = np.random.multivariate_normal(
-            np.random.normal(2, 1, 40), np.eye(40) * 1, 5
+            np.random.normal(2, 1, 39), np.eye(39) * 1, 200
         )
+        a_s = [another_samples[i : i + 40] for i in range(0, 161, 10)]
         for i in range(10):
-            print(dtw.predict(samples[i]))
+            print(dtw.predict_score(s[i]))
         print("another")
-        for s in another_samples:
-            print(dtw.predict(s))
+        for i in range(10):
+            print(dtw.predict_score(a_s[i]))
 
 
 unittest.main() if __name__ == "__main__" else None
