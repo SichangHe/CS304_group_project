67424f4149b6425d40f6978060f0e59c5eac2dc4
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-21 17:23:09 +0800

    HMM graph for unrestricted digits

---
+++ speech/project5/unrestricted_hmm.py
@@ -0,0 +1,29 @@
+"""Hidden Markov model from unrestricted digits."""
+
+import numpy as np
+
+from speech.project5.hmm import HMM_Single, HMMState, clone_hmm_states
+
+HALF_LOSS = -np.log(0.5)
+"""Negative log probability of a half."""
+
+
+def build_hmm_graph(digit_hmms: list[HMM_Single]):
+    """Connect HMM states to create the unrestricted number recognizer."""
+    non_emitting_state = HMMState.root()
+    # Defensively copy the input.
+    digit_state_list = [
+        clone_hmm_states(hmm_single.states) for hmm_single in digit_hmms
+    ]
+
+    for digit_states in digit_state_list:
+        digit_states[0].transition_loss[non_emitting_state] = 0.0
+        # Extra loss to discourage insertions.
+        non_emitting_state.transition_loss[digit_states[-1]] = (
+            digit_states[-1].exit_loss + HALF_LOSS
+        )
+
+    emitting_states = [
+        state for digit_states in digit_state_list for state in digit_states
+    ]
+    return [non_emitting_state], emitting_states
