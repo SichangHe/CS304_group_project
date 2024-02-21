df0ae942a1392229989a5149dcfe1ef6e7256034
Luyao Wang <lywlywly@gmail.com>
2024-02-21 13:43:24 +0800

    skeleton code for testing phone numbers

--- speech/project5/phone_rand.py
+++ speech/project5/phone_rand.py
@@ -1,6 +1,13 @@
 """Recognize 25 valid random telephone numbers and report accuracy.
 Run as `python3 -m speech.project5.phone_rand`."""
 
+from speech.project2.main import NUMBERS
+from speech.project3 import TEMPLATE_INDEXES
+from speech.project3.hmm import single_hmm_w_template_file_names
+from speech.project5.hmm import _align_sequence_and_hmm_states
+from speech.project5.phone_hmm import build_hmm_graph
+
+
 TELEPHONE_NUMBERS = [
     "8765",
     "2356",
@@ -36,7 +43,21 @@ def recording_for_number(number: str) -> str:
 
 
 def main() -> None:
-    print("TODO")
+    template_files = [
+        [f"recordings/{number}{i}.wav" for i in TEMPLATE_INDEXES] for number in NUMBERS
+    ]
+    single_hmms = [
+        single_hmm_w_template_file_names(t, n_states=5, n_gaussians=2)
+        for t in template_files
+    ]
+
+    non_emitting_states, emitting_states = build_hmm_graph(single_hmms)
+    # TODO:
+    for number_sequences in TELEPHONE_NUMBERS:
+        _align_sequence_and_hmm_states(
+            number_sequences, non_emitting_states, emitting_states
+        )
+        # TODO:
 
 
 main() if __name__ == "__main__" else None
