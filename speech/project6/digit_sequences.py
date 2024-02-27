c278a4c7af71c07a990ac8f2417c83b31102a1b7
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-27 11:36:22 +0800

    basics of training data

---
+++ speech/project6/digit_sequences.py
@@ -0,0 +1,22 @@
+"""Digit sequences for continuous speech recognition.
+Run as `python3 -m speech.project6.digit_sequences`"""
+
+SEQUENCES = [
+    "0123456789",
+    "9876543210",
+    "1234567890",
+    "0987654321",
+    "1357902468",
+    "8642097531",
+]
+"""Digit sequences for training."""
+
+INDEXES = range(5)
+"""Indexes for training sequences."""
+
+
+def main() -> None:
+    pass
+
+
+main() if __name__ == "__main__" else None
