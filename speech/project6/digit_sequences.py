1326f6336b4dd3f46689d643d52bf1dc137099cf
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-27 18:25:08 +0800

    record all project 6 training data

--- speech/project6/digit_sequences.py
+++ speech/project6/digit_sequences.py
@@ -2,10 +2,12 @@
 Run as `python3 -m speech.project6.digit_sequences`"""
 
 from os import system
+from queue import Queue
+from typing import Final
 
-from speech.project5.silence import record_audio
+from speech.project1.main import audio_recording_thread
 
-SEQUENCES = [
+SEQUENCES: Final = [
     "0123456789",
     "9876543210",
     "1234567890",
@@ -15,20 +17,19 @@ SEQUENCES = [
 ]
 """Digit sequences for training."""
 
-INDEXES = range(5)
+INDEXES: Final = range(5)
 """Indexes for training sequences."""
+
+SEQUENCE_FILE_NAMES: Final = [
+    f"recordings/{sequence}_{index}.wav" for sequence in SEQUENCES for index in INDEXES
+]
 
 
 def main() -> None:
-    file_names = (
-        f"recordings/{sequence}_{index}.wav"
-        for sequence in SEQUENCES
-        for index in INDEXES
-    )
-    for out_file_name in file_names:
+    for out_file_name in SEQUENCE_FILE_NAMES:
         while True:
             print(f"Recording {out_file_name}.")
-            record_audio(out_file_name)
+            audio_recording_thread(Queue(), out_file_name)
 
             system(f"open {out_file_name}")
             if input("Press Enter to record the next one.") == "":
