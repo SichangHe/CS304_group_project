61e19c5ee997c1fb0fc10cfb4825daaa3154eea0
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-27 16:05:37 +0800

    script to record sequences

--- speech/project6/digit_sequences.py
+++ speech/project6/digit_sequences.py
@@ -1,6 +1,10 @@
 """Digit sequences for continuous speech recognition.
 Run as `python3 -m speech.project6.digit_sequences`"""
 
+from os import system
+
+from speech.project5.silence import record_audio
+
 SEQUENCES = [
     "0123456789",
     "9876543210",
@@ -16,7 +20,19 @@ INDEXES = range(5)
 
 
 def main() -> None:
-    pass
+    file_names = (
+        f"recordings/{sequence}_{index}.wav"
+        for sequence in SEQUENCES
+        for index in INDEXES
+    )
+    for out_file_name in file_names:
+        while True:
+            print(f"Recording {out_file_name}.")
+            record_audio(out_file_name)
+
+            system(f"open {out_file_name}")
+            if input("Press Enter to record the next one.") == "":
+                break
 
 
 main() if __name__ == "__main__" else None
